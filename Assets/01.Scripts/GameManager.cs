94c6fa16837d54fbe168eb08dc158f4a64c50102
minhyeok <minhyeogjeon945@gmail.com>
2023-08-04 20:08:10 +0900

    GameManager

--- Assets/01.Scripts/GameManager.cs
+++ Assets/01.Scripts/GameManager.cs
@@ -2,17 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManger : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoints;
 
-    // Update is called once per frame
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    public GameObject player;
+
     void Update()
     {
-        
+        curSpawnDelay += Time.deltaTime;
+
+        if (curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            maxSpawnDelay = Random.Range(1f, 3f);
+            curSpawnDelay = 0;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        int ranEnemy = Random.Range(0, 3);
+        int ranPoint = Random.Range(0, 9);
+
+        GameObject enemy = Instantiate(enemyObjs[ranEnemy],
+                                       spawnPoints[ranPoint].position,
+                                       spawnPoints[ranPoint].rotation);
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        enemyLogic.player = player;
+
+        if (ranPoint == 5 || ranPoint == 6)
+        {
+            enemy.transform.Rotate(Vector3.back * 90);
+            rigid.velocity = new Vector2(enemyLogic.enemySpeed * (-1), -1);
+        }
+        else if (ranPoint == 7 || ranPoint == 8)
+        {
+            enemy.transform.Rotate(Vector3.forward * 90);
+            rigid.velocity = new Vector2(enemyLogic.enemySpeed, -1);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(0, enemyLogic.enemySpeed * (-1));
+        }
+    }
+
+    // 1.5ÃÊ ¹«Àû
+    public void RespawnPlayer()
+    {
+        StartCoroutine(RespawnPlayerExt());
+    }
+
+    IEnumerator RespawnPlayerExt()
+    {
+        yield return new WaitForSeconds(1.5f);
+        player.transform.position = new Vector3(0, -3.5f, 0);
+        player.SetActive(true);
     }
 }
